ffaa48c5b5763f71f1e59a4acbb6abe43cd36dad
Felipa Mendez <felipalouise@gmail.com>
2022-01-17 22:31:38 -0600

    commit tree 7d4c00c44a6e005fd26e0ab70e02943760598cf6

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+
+
+function superbowlWin(records) {
+    const superBowlWinDeclaration = records.find(record => record.result === "W")
+    if(superBowlWinDeclaration) {
+        return superBowlWinDeclaration.year
+      } else {
+        return undefined
+   }
+}
+
+
